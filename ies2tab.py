57f78a6c7f3cf63b41661679070f3d7511eabca3
aubema <aubema@localhost>
2016-02-16 11:38:17 -0500

    debugging ies2fcte m

---
+++ ies2tab.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# cedi est un commentaire
+import numpy as np
+
+filename = raw_input("IES filename : ")
+with open(filename) as f:
+	data = f.readlines()
+data = filter(None,map(str.strip, data))
+data = filter(lambda s: s[0].isdigit(), data)
+data = sum(map(lambda s: s.split(),data),[])
+
+nbV  = int(data[3])
+nbH  = int(data[4])
+Vang = data[13:13+nbV]
+Hang = data[13+nbV:13+nbV+nbH]
+data = map(None,*[iter(data[13+nbV+nbH:])]*nbH)
+
+outname = raw_input("out filename : ")
+with open(outname,'w') as f:
+	f.write("%d %d\n" % (nbV,nbH))
+	f.write(' '.join(Vang)+'\n')
+	f.write(' '.join(Hang)+'\n')
+	f.write('\n'.join([' '.join(d) for d in data])+'\n')
