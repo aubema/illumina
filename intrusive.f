8b89483137ab07f3f00d97e09e5f48d9b339fcad
Martin Aub? <martin.aube@cegepsherbrooke.qc.ca>
2016-05-05 17:10:24 +0000

    intrusive.f edited online with Bitbucket

--- intrusive.f
+++ intrusive.f
@@ -156,7 +156,7 @@ c ther= projection angle of the window as seen from the center of the right half
                 ther=asin((d_o(i,j)/2.*1.5)/
      +          sqrt((d_o(i,j)/2.*1.5)**2.+h_w**2.))
 c thef= projection angle of the window as seen from the center of the opposite facades
-                thef=asin(abs(h_o-h_w)/
+                thef=asin(abs(h_o(i,j)-h_w)/
      +          sqrt(d_o(i,j)**2.+(h_o(i,j)-h_w)**2.))
 c xif= projection angle of the facade as seen from the lamp
                 xif=abs((z_o+z_g)/2.-pi/2.)
