f663ba87050f12f70fa2134c68beb36458184c9b
asimoneau <asimoneau@localhost>
2016-06-03 14:44:43 -0400

    find-failed-runs bug fix

--- find-failed-runs.bash
+++ find-failed-runs.bash
@@ -27,7 +27,7 @@ done
 
 find $dir -name wl* | grep -v gridmap | while read dirname
 do
-    n=`tail -n 4 $dirname/Hawaii.out 2>/dev/null | head -n 1 | grep -E 'E(\+|\-)' | wc -l`
+    n=`tail -n 4 $dirname/*.out 2>/dev/null | head -n 1 | grep -E 'E(\+|\-)' | wc -l`
     if [ $n -eq 0 ]
     then
         if [ -z $full_out ]
