de3cd9d0e5865553d2b247d363aee42c01d8ebd3
Alexandre Simoneau <alsimoneau@gmail.com>
2018-09-22 16:16:25 -0400

    manyruns updated to SLURM

--- manyruns.bash
+++ manyruns.bash
@@ -12,15 +12,15 @@ do  bash $i
     echo $i
     echo "===="
     user=`whoami`
-    njob=`bqstat 2>/dev/null | grep -c $user`
-    nwait=`bqstat 2>/dev/null | grep $user | grep -c " Q "`
+    njob=`squeue -u $user -h | grep -c ""`
+    nwait=`squeue -u $user -h -o "%t" | grep -c "PD"`
     echo $njob "initial"
     while  [ $njob -gt 1000 ] && [ $nwait -gt 100 ]
     do 
-       njob=`bqstat 2>/dev/null | grep -c $user`
-       nwait=`bqstat 2>/dev/null | grep $user | grep -c " Q "`
-       echo $njob $nwait
-       sleep 30 
+        njob=`squeue -u $user -h | grep -c ""`
+        nwait=`squeue -u $user -h -o "%t" | grep -c "PD"`
+        echo $njob $nwait
+        sleep 30 
     done
 done
 echo "End of multiple experiments"
