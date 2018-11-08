395c9ed39801884ac8b0ac179f06b4271e487d4f
Martin Aube <martin.aube@cegepsherbrooke.qc.ca>
2018-11-07 22:59:59 -0500

    passer de double a simple dans les in out

--- 2dout.f
+++ 2dout.f
@@ -1,7 +1,7 @@
        subroutine doubleout(nbx,nby,filename,bindata)
 c write double precision array in binary
        integer nbx,nby,i,j
-       real*8 bindata(1024,1024)
+       real bindata(1024,1024)
        character*72 filename
        open(unit=1,form='unformatted',file=filename,action='write')
          do j=1,nby
