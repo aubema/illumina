6859e47035025a9cfcfec779ebff900d69666805
Alexandre Simoneau <alsimoneau@gmail.com>
2019-01-21 12:12:42 -0500

    Added initialisation script

---
+++ init_run.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import shutil
+import os
+from glob import glob
+
+ppath = os.environ['PATH'].split(os.pathsep)
+illumpath = filter(lambda s: "illumina" in s and "bin" not in s, ppath)[0]
+
+if not os.path.exists("Lights"):
+    shutil.copytree(illumpath+"/Example/Lights",'Lights')
+
+example_files = glob(illumpath+"/Example/*.*")
+files = filter(lambda s: not(s.endswith("bin") or s.endswith("csv")), example_files)
+
+for filename in files:
+    shutil.copy2(filename,'.')
