50c55cd404f4d3d51c33071c1305b199cff239ac
Alexandre Simoneau <alsimoneau@gmail.com>
2019-02-04 17:57:25 -0500

    Removed fake class in Illuminutils

--- Illuminutils.py
+++ Illuminutils.py
@@ -27,43 +27,31 @@ def MYD09A1_band_name(fname, band_n):
     sub_ds = gdal.Open(fname).GetSubDatasets()
     return next( s[0] for s in sub_ds if ("b%02d" % band_n) in s[0] )
 
-class Illuminutils:
-    def __init__(self, ini):
-        with open(ini) as f:
-            self.params = yaml.load(f)
+def save(params, data, dstname, scale_factor=1.):
+    scaled_data = [ d*scale_factor for d in data ]
+    ds = hdftools.from_domain(params,data)
+    ds.save(dstname)
 
-    def save(self, data, dstname, scale_factor=1.):
-        scaled_data = [ d*scale_factor for d in data ]
-        ds = hdftools.from_domain(self.params,data)
-        ds.save(dstname)
+with open(glob("*.ini")[0]) as f:
+    params = yaml.load(f)
 
-    def srtm(self, folder):
-        files = glob(folder+"/*.hgt")
-        print "    ".join(map(str,files))
-        data = [ warp(files, self.params['srs'], extent) \
-            for extent in self.params['extents'] ]
-        self.save(data,"srtm")
+files = glob("SRTM/*.hgt")
+print "    ".join(map(str,files))
+data = [ warp(files, params['srs'], extent) \
+    for extent in params['extents'] ]
+save(params, data, "srtm")
 
-    def modis(self, folder, band_n):
-        files = glob(folder+"/*.hdf")
-        fname = "refl_b%02d" % band_n
-        band_names = map( lambda f: MYD09A1_band_name(f, band_n), files )
-        print "    ".join(map(str,band_names))
-        data = [ warp(band_names, self.params['srs'], extent) \
-            for extent in self.params['extents'] ]
-        self.save(data, fname, scale_factor=0.0001)
+for band_n in xrange(1,8):
+    files = glob("MODIS/*.hdf")
+    fname = "refl_b%02d" % band_n
+    band_names = map( lambda f: MYD09A1_band_name(f, band_n), files )
+    print "    ".join(map(str,band_names))
+    data = [ warp(band_names, params['srs'], extent) \
+        for extent in params['extents'] ]
+    save(params, data, fname, scale_factor=0.0001)
 
-    def viirs(self, folder):
-        files = glob(folder+"/*.tif")
-        print "    ".join(map(str,files))
-        data = [ warp(files, self.params['srs'], extent) \
-            for extent in self.params['extents'] ]
-        self.save(data,"stable_lights")
-
-if __name__ == "__main__":
-    I = Illuminutils(glob("*.ini")[0])
-
-    I.srtm("SRTM")
-    I.viirs("VIIRS-DNB")
-    for i in range(1,8):
-        I.modis("MODIS",i)
+files = glob("VIIRS-DNB/*.tif")
+print "    ".join(map(str,files))
+data = [ warp(files, params['srs'], extent) \
+    for extent in params['extents'] ]
+save(params, data, "stable_lights")
