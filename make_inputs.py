Fixed normalisation

make_inputs.py
@@ -74,13 +74,16 @@ lop = {
 
 # Spectral distribution (normalised with scotopric vision to 1)
 wav, viirs = np.loadtxt( "Lights/viirs.dat", skiprows=1 ).T
-viirs = pt.spct_norm(wav,viirs)
-norm_spectrum = pt.load_spct(
-	wav,
-	np.ones(wav.shape),
-	"Lights/photopic.dat",
-	1
-)
+viirs /= np.max(viirs)
+#viirs = pt.spct_norm(wav,viirs)0000000000000000000
+# norm_spectrum = pt.load_spct(
+# 	wav,
+# 	np.ones(wav.shape),
+# 	"Lights/photopic.dat",
+# 	1
+# )
+wav, norm_spectrum = np.loadtxt("Lights/photopic.dat", skiprows=1).T
+norm_spectrum /= np.max(norm_spectrum)
 
 spct_files = glob("Lights/*.spct")
 spct = {

make_zones.py
@@ -64,8 +64,9 @@ S = np.array([
 	for i in xrange(len(viirs_dat)) ])
 
 # phie = DNB * S / int( R ( rho/pi Gdown + Gup ) ) dlambda
-Gdown = (zones * sinx[:,None])[:,:,angles>90].sum(2)
-Gup = (zones * sinx[:,None])[:,:,angles<70].sum(2) / sinx[angles<70].sum()
+Gdown = np.tensordot(zones[:,:,angles>90],sinx[angles>90],axes=([2],[0]))
+Gup = np.tensordot(zones[:,:,angles<70],sinx[angles<70],axes=([2],[0])) \
+	/ sinx[angles<70].sum()
 integral = np.sum(viirs * (Gdown*refl/np.pi + Gup),(1,2)) * (wav[1]-wav[0])
 
 phie = [