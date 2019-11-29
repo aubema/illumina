824f047a7f49313c34e8be992c3d73035ee277cc
aubema <aubema@localhost>
2019-11-29 17:49:23 +0000

    virtual volume for 2n scat

--- illumina.f
+++ illumina.f
@@ -253,7 +253,7 @@ c reading of the fichier d'entree (illumina.in)
       if (ssswit.eq.0) then
         effdif=0.
       else
-        effdif=30000.
+        effdif=25000.
         n2nd=10000
       endif
       if (dx.lt.siz) siz=dx                                               ! chose the minimum between dx and 200m.
