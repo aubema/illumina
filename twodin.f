edf2fc1a93add1d37528f12db4cdb78d7e72f5bd
aubema <aubema@localhost>
2019-12-01 18:52:17 +0000

    restoring intrants and extrants

--- twodin.f
+++ twodin.f
@@ -1,13 +1,16 @@
        subroutine twodin(nbx,nby,filename,bindata)
+       integer width                                                      ! Matrix dimension in Length/width and height
+       parameter (width=256)
 c read double precision array in binary
        integer nbx,nby,i,j
-       real bindata(256,256)
+       real bindata(width,width)
        character*72 filename
        open(unit=1,form='unformatted',file=filename,action='read')
          read(1) nbx,nby
          if ((nbx.gt.width).or.(nby.gt.width)) then
           print*,'You try to use a domain larger than the maximum'
           print*,'allowed. Please restrict it to no more that 256 x 256'
+          print*,'Your domain size is: ',nbx,'x',nby
           print*,'Computation aborted'
           stop
          endif
