593291531eed1b02b2a3958e4851ccc766e91556
aubema <aubema@localhost>
2019-12-02 20:09:39 +0000

    variable siz with addition of dx/1000

--- illumina.f
+++ illumina.f
@@ -249,12 +249,12 @@ c reading of the fichier d'entree (illumina.in)
         read(1,*) cloudt, cloudbase
         read(1,*) 
       close(1)
-      siz=1000.
+      siz=200.
       if (ssswit.eq.0) then
         effdif=0.
       else
         effdif=100000.
-        n2nd=50000
+        n2nd=1000
       endif
 c      siz=siz+dx/1000.
       omemax=1./((siz)**2.)                                                 ! about two degree
