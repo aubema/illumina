f4a368fff1cf5849ed7682a83023bc0c64d0e49c
Alexandre Simoneau <alsimoneau@gmail.com>
2020-04-02 18:31:15 -0400

    Fixed version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='illum',
-    version='2.5',
+    version='2.0.1',
     py_modules=['main','defineDomain','Illuminutils','make_inputs',
     'make_zones','make_lamps','makeBATCH','find-failed-runs','extract-output-data',
     'init_run','alt_scenario_maker','hdf_convert','pytools','MultiScaleData'],
