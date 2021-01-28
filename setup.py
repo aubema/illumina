7de16e3a4013f508af53d02253191de78b77fe96
Alexandre Simoneau <alsimoneau@gmail.com>
2021-01-28 13:03:46 -0500

    2.1.21w04.4b New CLI

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='illum',
-    version='2.1.21w04.4a-dev',
+    version='2.1.21w04.4b',
     py_modules=['main','defineDomain','Illuminutils','make_inputs',
     'make_zones','make_lamps','makeBATCH','find-failed-runs','extract-output-data',
     'init_run','alt_scenario_maker','hdf_convert','pytools','MultiScaleData'],
