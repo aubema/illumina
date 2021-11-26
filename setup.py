218668d894bb9b4159421602b4e7ca490a1b28fb
Alexandre Simoneau <alsimoneau@gmail.com>
2021-11-26 14:08:14 +0000

    2.1.21w47.5a-dev: Updated molecular optical depth to Bodhaine (1999)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='illum',
-    version='2.1.21w46.1b-dev',
+    version='2.1.21w47.5a-dev',
     py_modules=[
         'main',
         'alt_scenario_maker',
