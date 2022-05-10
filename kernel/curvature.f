0bccee6020791511b2302b5f4a451fce2d0d5698
Martin Aube <martin.aube@cegepsherbrooke.qc.ca>
2022-05-10 14:38:58 -0400

    2.2.22w19.2a-illum-health: menage

--- kernel/curvature.f
+++
@@ -1,9 +0,0 @@
-c  adding earth curvature to the topography
-c
-      subroutine curvature(distc,hcur)
-      real Rearth,distc,hcur
-      Rearth=6371000.  
-      hcur=Rearth-sqrt(Rearth**2.-distc**2.)
-      hcur=-1.*hcur
-      return
-      end
