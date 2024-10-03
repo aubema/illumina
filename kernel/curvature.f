181a14389bfdaf170dced541cb2056ee8411de5b
aubema <60398833+aubema@users.noreply.github.com>
2024-10-03 14:01:33 -0400

    Update curvature.f
    sqrt in double

--- kernel/curvature.f
+++ kernel/curvature.f
@@ -3,7 +3,7 @@ c
       subroutine curvature(distc,hcur)
       real*8 Rearth,distc,hcur
       Rearth=6371000.D0  
-      hcur=Rearth-sqrt(Rearth**2.-distc**2.)
+      hcur=Rearth-dsqrt(Rearth**2.-distc**2.)
       hcur=-1.*hcur
       return
       end
